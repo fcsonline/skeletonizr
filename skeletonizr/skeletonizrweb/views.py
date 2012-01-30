4f352d06badc9b3c5d9e8e8d6a22e1614a795500
Ferran Basora <fcsonline@gmail.com>
2012-01-31 00:35:00 +0100

    Delete temp project folder. Generate capitalized entities

--- skeletonizr/skeletonizrweb/views.py
+++ skeletonizr/skeletonizrweb/views.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 import datetime
-import os, sys, shutil 
+import os, sys, shutil
 
 def index(request):
     data = {}
@@ -22,6 +22,8 @@ def gen(request):
 
     path = '/tmp/' + project + '/'
 
+    shutil.rmtree(path)
+
     if not os.path.exists(path):
       os.makedirs(path)
 
@@ -33,43 +35,56 @@ def gen(request):
     tpl_dir = './skeletonizrweb/templates/'
 
     for dirname, dirnames, filenames in os.walk(tpl_dir + 'nodeexpress/'):
-        for subdirname in dirnames:
-            src = os.path.join(dirname, subdirname)
-            src_tpl = os.path.join(dirname, subdirname)[len(tpl_dir):]
-            dst_tpl = os.path.join(dirname, subdirname)[len(tpl_dir) + len('nodeexpress/'):]
-            dst = os.path.join(path, dst_tpl)
+      for subdirname in dirnames:
+        src = os.path.join(dirname, subdirname)
+        src_tpl = os.path.join(dirname, subdirname)[len(tpl_dir):]
+        dst_tpl = os.path.join(dirname, subdirname)[len(tpl_dir) + len('nodeexpress/'):]
+        dst = os.path.join(path, dst_tpl)
 
-            log += 'Creating ' + dst + '\n'
-      	    os.makedirs(dst)
-        for filename in filenames:
-            src = os.path.join(dirname, filename)
-            src_tpl = os.path.join(dirname, filename)[len(tpl_dir):]
-            dst_tpl = os.path.join(dirname, filename)[len(tpl_dir) + len('nodeexpress/'):]
-            dst = os.path.join(path, dst_tpl)
+        log += 'Creating ' + dst + '\n'
+        os.makedirs(dst)
 
-	    if dst.find('__entity__') >= 0:
-            	log += 'Entity in ' + dst + '\n'
+      for filename in filenames:
+        src = os.path.join(dirname, filename)
+        src_tpl = os.path.join(dirname, filename)[len(tpl_dir):]
+        dst_tpl = os.path.join(dirname, filename)[len(tpl_dir) + len('nodeexpress/'):]
+        dst = os.path.join(path, dst_tpl)
 
-	        for entity in entities:
-	            data['entity'] = entity
+        if is_magic_path(dst):
+          log += 'Entity in ' + dst + '\n'
 
-		    dst_entity = dst.replace('__entity__', entity)
+          for entity in entities:
+            data['entity'] = entity
 
-		    f = open(dst_entity, 'w')
-		    source = render_to_string(src_tpl, data)
-		    f.write(source)
-		    f.close()
-	    else:
-                log += 'Generating ' + dst + '\n'
+            dst_entity = get_magic_path (dst, data)
 
-	        f = open(dst, 'w')
-	        source = render_to_string(src_tpl, data)
-	        f.write(source)
-	        f.close()
+            f = open(dst_entity, 'w')
+            source = render_to_string(src_tpl, data)
+            f.write(source)
+            f.close()
+        else:
+          log += 'Generating ' + dst + '\n'
+
+          f = open(dst, 'w')
+          source = render_to_string(src_tpl, data)
+          f.write(source)
+          f.close()
 
     #return HttpResponse(log, content_type='text/plain')
     #return HttpResponse(log, content_type="text/plain")
     return HttpResponse(log)
 
-def genserver():
-  return "Hola"
+def is_magic_path(filename):
+
+  if filename.find('__entity__') >= 0:
+    return True
+
+  if filename.find('__Entity__') >= 0:
+    return True
+
+  return False
+
+def get_magic_path(filename, data):
+  filename = filename.replace('__entity__', data['entity'])
+  filename = filename.replace('__Entity__', data['entity'].capitalize())
+  return filename
